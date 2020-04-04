010d93338fd84bb60c608d20b9d847d4046e8330
PauloHPS1991 <30605988+PauloHPS1991@users.noreply.github.com>
2020-04-04 15:22:27 -0300

    Create testa_cadastro.php

---
+++ testa_cadastro.php
@@ -0,0 +1,8 @@
+<form action="cadastrar.php" method="POST">
+	Nome: <input type="text" name="nome_app"><br />
+	Telefone: <input type="text" name="telefone_app"><br />
+	E-mail: <input type="text" name="email_app"><br />
+	Cartao: <input type="text" name="cartao_app"><br />
+	
+	<input type="submit" value="Cadastrar">
+</form>
